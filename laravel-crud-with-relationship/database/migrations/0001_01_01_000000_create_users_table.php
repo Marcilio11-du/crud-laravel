298de34048995a6b53dfa06a7ec14a65e92911c5
marcilio11-du <marcilio11du@gmail.com>
2026-04-29 10:49:42 +0100

    feat(migration): refactor the migration of users table
    - create the migration to create categories table
    - create the migratio to create articles table
    - create the migratio to create articles_users table

--- laravel-crud-with-relationship/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel-crud-with-relationship/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('p_name');
+            $table->string('p_surname');
+            $table->date('birth-date')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
