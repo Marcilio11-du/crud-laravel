b550562afd475a1436d647c6cfacfe57bb959212
marcilio11-du <marcilio11du@gmail.com>
2026-05-05 08:03:18 +0100

    feat: create the routes to all necessary parts of the program

--- laravel-crud-with-relationship/routes/web.php
+++ laravel-crud-with-relationship/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rotas da Lixeira (devem vir ANTES do resource para não serem capturadas como {article})
+Route::get('articles/trash', [ArticleController::class, 'trash'])->name('articles.trash');
+Route::patch('articles/{id}/restore', [ArticleController::class, 'restore'])->name('articles.restore');
+
+// Recurso Principal
+Route::resource('article', ArticleController::class);
